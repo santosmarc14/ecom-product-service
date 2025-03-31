8eecb0a3611f6f2a257599bbfa8e505a220b1bcd
Marc Benedict Santos <s.marcbenedict@gmail.com>
2025-03-31 12:26:57 +0800

    feat: product model and controller / api

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'price' => fake()->randomFloat(2,1,1000),
+            'stock' => fake()->numberBetween(0,100),
+            'sku' => fake()->word()
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('token')->group(function () {
+    Route::post('/test', function () {
+        return response()->json(
+            [
+                'message' => 'Token is valid'
+            ]
+        );
+    });
+
+    Route::post('/products', ProductController::class)->name('products.list');
+});
